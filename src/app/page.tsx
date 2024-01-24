810ef7bcf12147cacec9670f6dade28878521c96
LeoLizc <leolizcanop@gmail.com>
2024-01-24 16:34:43 -0500

    Add Badge component and update Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Badge from '@/ui/components/badge';
+import SectionLayout from '@/ui/components/section-layout';
+
 export default function Home() {
   return (
     <main className="">
-      hello world
+      <SectionLayout>
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold flex flex-row gap-5 items-center mb-4">
+          ¡Hola! Soy Leonardo
+          <Badge
+            text="email"
+            hiddenText="ldlizcano@uninorte.edu.co"
+          />
+        </h1>
+        <h2>
+          Ingeniero de sistemas. Desarrollador web backend con conocimientos en frontend.
+          Y aficionado a la inteligencia artificial
+        </h2>
+      </SectionLayout>
     </main>
   );
 }
---
+++ src/ui/components/badge/index.tsx
@@ -0,0 +1,25 @@
+import './badge.css';
+
+type BadgeProps = {
+  text: string;
+  hiddenText?: string;
+};
+
+export default function Badge({ text, hiddenText }: BadgeProps) {
+  const defClassName = 'bg-indigo-100 text-indigo-800 text-xs font-medium me-2 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400';
+  const hiddenClassName = 'px-2.5 border border-indigo-400';
+  return (
+    <div className="text-base badge">
+      <span className={`${defClassName} ${hiddenClassName} px-2.5 visible-text${hiddenText == null ? ' ' : ' animated'}`}>
+        {text}
+      </span>
+      {hiddenText && (
+        <div className="hidden-text-container">
+          <span className={`${defClassName} hidden-text`}>
+            {hiddenText}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
