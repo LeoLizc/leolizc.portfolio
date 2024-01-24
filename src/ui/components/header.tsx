972576015a1ebe9535a36d5d154dafbf06766dc0
LeoLizc <leolizcanop@gmail.com>
2024-01-23 22:53:04 -0500

    Add header component and update global styles

---
+++ src/ui/components/header.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <header className="flex flex-row justify-between w-full lg:w-[1020px]">
+      <Image src="/imgs/avatar.webp" width="48" height="48" alt="avatar" />
+      <nav className="flex flex-row items-center gap-6">
+        <a href="/#experiencia">Experiencia</a>
+        <a href="/#proyectos">Proyectos</a>
+        <a href="/#sobre-mi">Sobre mí</a>
+        <a href="/#contacto">Contacto</a>
+      </nav>
+    </header>
+  );
+}
