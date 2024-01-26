3438d5c3702bfb102d5a205fae7ff2d6bceffd65
LeoLizc <leolizcanop@gmail.com>
2024-01-25 23:26:36 -0500

    Add className prop to Indicator component

--- src/ui/components/indicator.tsx
+++ src/ui/components/indicator.tsx
@@ -1,6 +1,7 @@
 type IndicatorProps = {
   children: React.ReactNode;
   isStatic?: boolean;
+  className?: string;
 };
 
 const onStaticClasses = [
@@ -8,10 +9,10 @@ const onStaticClasses = [
   'dark:bg-[linear-gradient(110deg,#164e63,45%,#1e2631,55%,#164e63)] bg-[length:200%_100%]',
 ];
 
-export default function Indicator({ children, isStatic = false }: IndicatorProps) {
+export default function Indicator({ children, isStatic = false, className = '' }: IndicatorProps) {
   return (
-    <span className={`inline-flex items-center bg-cyan-100 text-cyan-800 text-xs font-medium px-2.5 py-0.5 rounded-full ${onStaticClasses[+isStatic]} dark:text-cyan-200 drop-shadow-hilight-md`}>
-      <span className="w-2 h-2 me-1 bg-cyan-500 rounded-full" />
+    <span className={`inline-flex items-center bg-cyan-100 text-cyan-800 text-xs font-medium px-2.5 py-0.5 rounded-full ${onStaticClasses[+isStatic]} dark:text-cyan-200 drop-shadow-hilight-md ${className}`}>
+      {!isStatic && <span className="w-2 h-2 me-1 bg-cyan-500 rounded-full" />}
       {children}
     </span>
   );
