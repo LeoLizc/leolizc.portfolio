b0dd263fe729a1d2ecbd671bae2aeee27e1e61eb
LeoLizc <leolizcanop@gmail.com>
2024-01-26 11:21:33 -0500

    Update terminalIcon to TerminalIcon in icons.tsx

--- src/ui/components/icons.tsx
+++ src/ui/components/icons.tsx
@@ -34,7 +34,7 @@ export function MailIcon(props: SVGProps) {
   );
 }
 
-export function terminalIcon(props: SVGProps) {
+export function TerminalIcon(props: SVGProps) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-terminal" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
