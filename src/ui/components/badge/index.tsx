2ce1d61a4210e80fdde8a64bd19a07e051a2e8ca
LeoLizc <leolizcanop@gmail.com>
2024-01-27 00:07:17 -0500

    Refactor Badge component to separate anchor and div elements, and fixing SEO

--- src/ui/components/badge/index.tsx
+++ src/ui/components/badge/index.tsx
@@ -12,20 +12,33 @@ export default function Badge({
 }: BadgeProps) {
   const defClassName = 'bg-indigo-100 text-indigo-800 text-s font-medium py-0.5 rounded-md dark:bg-gray-700 dark:text-indigo-300';
   const hiddenClassName = 'px-2.5 border border-indigo-400';
-  return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className="text-base badge me-2 pointer-events-[fill]">
+  const content = (
+    <>
       <span className={`${defClassName} ${hiddenClassName} px-2.5 visible-text${hiddenText == null ? ' ' : ' animated'} flex gap-1`}>
         {children}
         {text}
       </span>
-      {hiddenText && (
-        <div className="hidden-text-container">
-          <span className={`${defClassName} hidden-text display-flex gap-1`}>
-            {/* {children} */}
-            {hiddenText}
-          </span>
-        </div>
-      )}
+      {
+        hiddenText && (
+          <div className="hidden-text-container">
+            <span className={`${defClassName} hidden-text display-flex gap-1`}>
+              {/* {children} */}
+              {hiddenText}
+            </span>
+          </div>
+        )
+      }
+    </>
+  );
+  // TODO: El anchor debería estar por fuera
+  // !El anchor debería estar por fuera
+  return (href != null) ? (
+    <a href={href} target="_blank" rel="noopener noreferrer" className="text-base badge me-2 pointer-events-[fill]">
+      {content}
     </a>
+  ) : (
+    <div className="text-base badge me-2 pointer-events-[fill]">
+      {content}
+    </div>
   );
 }
