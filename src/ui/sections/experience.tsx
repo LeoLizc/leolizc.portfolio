de5f6341104eec58382e14ff52c0aae3ee3c45cc
LeoLizc <leolizcanop@gmail.com>
2024-01-28 16:27:34 -0500

    Experience details increased

--- src/ui/sections/experience.tsx
+++ src/ui/sections/experience.tsx
@@ -4,14 +4,16 @@ const experiences = [
   {
     title: 'Desarrollador Backend - Eyeland Epics IEEE',
     date: 'Agosto 2022 - Septiembre 2023',
-    description: 'Responsable del **desarrollo backend** en Proyecto parte de la iniciativa EPICS en *IEEE*, que se propone apoyar el proceso de **aprendizaje del inglés** para personas con **discapacidad visual** a través de una aplicación móvil, en su objetivo de aumentar la inclusión.', // TODO: Corregir descripción
+    description: `Responsable del **desarrollo backend** en Proyecto parte de la iniciativa EPICS en *IEEE*, que se propone apoyar el proceso de **aprendizaje del inglés** para personas con **discapacidad visual** a través de una aplicación móvil, en su objetivo de aumentar la inclusión.\n
+Me encargué de *diseñar* y servir el sistema basado en **Sockets** para orquestar la comunicación y sincronización entre dispositivos y perfiles, así como la Web Api`,
     link: 'https://eyeland-project.github.io/webpage/',
     latest: true,
   },
   {
     title: 'Mentor en programación web - MisionTIC 2022',
     date: 'Septiembre 2022 - Octubre 2022',
-    description: '**Mentor en programación web** durante el tercer ciclo del programa MisionTIC 2022, liderado por el *Ministerio de las TIC* de Colombia y la Universidad del Norte.', // TODO: Corregir descripción
+    description: `**Mentor en programación web** durante el tercer ciclo del programa MisionTIC 2022, liderado por el *Ministerio de las TIC* de Colombia y la Universidad del Norte.\n
+Apoyé y resolví dudas a los estidantes acerca de el desarrollo de aplicaciones web con **Flask, Html y Sq-Lite**`,
   },
 ];
 
