6199794fd0fc119066c6493725d4cd6c8bc1603a
LeoLizc <leolizcanop@gmail.com>
2024-01-27 20:47:42 -0500

    Update Eyeland Projects component with new data

--- src/ui/sections/projects.tsx
+++ src/ui/sections/projects.tsx
@@ -4,10 +4,12 @@ export default function Projects() {
   return (
     <div className="grid grid-cols-12 auto-rows-[7rem] gap-4">
       <BentoItem
-        className="col-span-8 row-span-3"
-        title="Placeholder 1"
-        subtitle="Subtitle PlaceHolder"
-        description="Lorem ipsumdfsdfdsfs dffsfmkfndfg ndkgdfjgn ggnfmgnjdfsgndfn"
+        className="col-span-8 row-span-3 [&_h3]:text-3xl [&_h3]:text-[#2acf76]"
+        title="Epics - Eyeland"
+        imageSrc="/imgs/Eyeland_shots_so.webp"
+        imageAlt="Página wev de Eyeland"
+        subtitle="Desarrolador - Backend"
+        description="Programa educativo para estudiantes de secundaria para la construcción de aplicaciones móviles para promover la inclusión de personas con discapacidad visual."
       />
       <BentoItem
         className="col-span-4 row-span-3"
