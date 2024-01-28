709c28d87c0607530b2d7276917900147874b2f5
LeoLizc <leolizcanop@gmail.com>
2024-01-28 13:41:12 -0500

    about me styling text

--- src/ui/sections/about-me.tsx
+++ src/ui/sections/about-me.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable max-len */
 import Image from 'next/image';
 
 export default function AboutMe() {
@@ -10,21 +12,17 @@ export default function AboutMe() {
         src="/imgs/avatar.webp"
         alt="Foto de Leo"
       />
-      <div className="[&>p]:mb-4 md:[&>p]:text-right [&>p]:text-left [&>p]:font-medium">
+      <div className="[&>p]:mb-4 md:[&>p]:text-right [&>p]:text-left [&>p]:font-medium
+      [&>p>strong]:text-yellow-300 [&>p>em]:text-cyan-400"
+      >
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem magnam labore
-          aspernatur at facilis dolore pariatur, commodi, itaque nobis, ipsam dignissimos
-          architecto voluptate ex hic recusandae necessitatibus officiis sapiente soluta?
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem <strong>magnam labore aspernatur at facilis dolore pariatur</strong>, commodi, itaque nobis, ipsam dignissimos architecto voluptate ex hic recusandae necessitatibus officiis sapiente soluta?
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda alias
-          temporibus, adipisci amet tempora perspiciatis atque necessitatibus eveniet quod
-          numquam eaque nam esse expedita laborum veniam deserunt vitae officia maxime!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. <strong>Assumenda alias temporibus</strong>, adipisci amet tempora perspiciatis atque necessitatibus eveniet quod numquam eaque nam esse expedita laborum <strong>veniam deserunt vitae officia maxime!</strong>
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda alias
-          temporibus, adipisci amet tempora perspiciatis atque necessitatibus eveniet quod
-          numquam eaque nam esse expedita laborum veniam deserunt vitae officia maxime!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda alias temporibus, <strong>adipisci amet tempora</strong> perspiciatis atque necessitatibus eveniet quod <strong>numquam eaque nam esse</strong> expedita laborum veniam deserunt vitae officia maxime!
         </p>
       </div>
     </section>
