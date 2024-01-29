ddb6057cdec4919d7211f613319e51da6d95261e
LeoLizc <leolizcanop@gmail.com>
2024-01-29 01:28:24 -0500

    fade animation added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${onest.className}`}>
+      <body className={`${onest.className} animate-fade animate-duration-[1200ms] animate-delay-[200ms] animate-ease-in-out animate-fill-forwards`}>
         <div className="absolute top-0 bottom-0 z-[-2] min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_90%_at_50%_-27%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
         <Header />
         {children}
