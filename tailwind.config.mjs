cb0aac0fbc9fa5ff091c12dcbfee376ea02d996b
LeoLizc <leolizcanop@gmail.com>
2024-01-30 01:52:04 -0500

    set default dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@
 import animations from 'tailwindcss-animated';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
