83cc1e84bc26c75ae232b8f80de593ef70b9cda5
LeoLizc <leolizcanop@gmail.com>
2024-01-31 18:58:48 -0500

    Add Spanish translation for main page

---
+++ src/i18n/translations/es.ts
@@ -0,0 +1,21 @@
+const es = {
+  main: {
+    SEO: {
+      title: 'Portfolio LeoLizc | Ingeniero de Sistemas',
+      description: 'Portafolio de Leonardo Lizcano, Ingeniero de Sistemas de la Universidad Nacional de Colombia',
+    },
+    PHOTO_ALT: 'Foto de Leonardo Lizcano',
+    TITLE: '¡Hola! Soy Leonardo',
+    AVAILABLE_TEXT: 'Disponible para trabajar',
+    PRESENTATION: 'Ingeniero de sistemas titulado. Desarrollador web backend con conocimientos en frontend. Y aficionado a la inteligencia artificial.',
+    EXPERIENCE_TITLE: 'Experiencia',
+    PROJECTS_TITLE: 'Proyectos',
+    ABOUT_TITLE: 'Sobre mí',
+    CONTACT_TITLE: 'Contacto',
+
+  }
+};
+
+export type Lang = typeof es;
+
+export default es;
