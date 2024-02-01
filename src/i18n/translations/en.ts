7e5e070955d0f9415358782e94c29056c3c82c2c
LeoLizc <leolizcanop@gmail.com>
2024-01-31 19:45:30 -0500

    Add internationalization support for Experience

--- src/i18n/translations/en.ts
+++ src/i18n/translations/en.ts
@@ -1,6 +1,8 @@
 import {type Lang} from './es'
 
 export const en: Lang = {
+  EXP_ITEM_GOTO: "Go to see",
+  EXP_ITEM_RECENT: "Most recent",
   main: {
     SEO: {
       title: "Portfolio LeoLizc | Software Engineer",
@@ -14,6 +16,24 @@ export const en: Lang = {
     PROJECTS_TITLE: "Projects",
     ABOUT_TITLE: "About",
     CONTACT_TITLE: "Contact Me",
+    EXPERIENCES: [
+      {
+        title: "Backend Developer - Eyeland Epics IEEE",
+        date: "August 2022 - September 2023",
+        description: `Responsible for the **backend development** in a project part of the EPICS initiative in *IEEE*, which aims to support the **English learning** process for people with **visual disabilities** through a mobile application, in its goal of increasing inclusion.
+
+I was in charge of *designing* and serving the system based on **Sockets** to orchestrate communication and synchronization between devices and profiles, as well as the Web Api`,
+        link: "https://eyeland-project.github.io/webpage/",
+        latest: true,
+      },
+      {
+        title: "Web programming mentor - MisionTIC 2022",
+        date: "September 2022 - October 2022",
+        description: `**Web programming mentor** during the third cycle of the MisionTIC 2022 program, led by the *Ministry of ICT* of Colombia and the Universidad del Norte.
+
+I supported and solved doubts for students about the development of web applications with **Flask, Html and Sq-Lite**`,
+      },
+    ],
   }
 }
 
--- src/i18n/translations/es.ts
+++ src/i18n/translations/es.ts
@@ -1,4 +1,6 @@
 const es = {
+  EXP_ITEM_GOTO: 'Ir a ver',
+  EXP_ITEM_RECENT: 'Más reciente',
   main: {
     SEO: {
       title: 'Portfolio LeoLizc | Ingeniero de Sistemas',
@@ -12,7 +14,24 @@ const es = {
     PROJECTS_TITLE: 'Proyectos',
     ABOUT_TITLE: 'Sobre mí',
     CONTACT_TITLE: 'Contacto',
-
+    EXPERIENCES: [
+      {
+        title: 'Desarrollador Backend - Eyeland Epics IEEE',
+        date: 'Agosto 2022 - Septiembre 2023',
+        description: `Responsable del **desarrollo backend** en Proyecto parte de la iniciativa EPICS en *IEEE*, que se propone apoyar el proceso de **aprendizaje del inglés** para personas con **discapacidad visual** a través de una aplicación móvil, en su objetivo de aumentar la inclusión.
+    
+Me encargué de *diseñar* y servir el sistema basado en **Sockets** para orquestar la comunicación y sincronización entre dispositivos y perfiles, así como la Web Api`,
+        link: 'https://eyeland-project.github.io/webpage/',
+        latest: true,
+      },
+      {
+        title: 'Mentor en programación web - MisionTIC 2022',
+        date: 'Septiembre 2022 - Octubre 2022',
+        description: `**Mentor en programación web** durante el tercer ciclo del programa MisionTIC 2022, liderado por el *Ministerio de las TIC* de Colombia y la Universidad del Norte.
+    
+Apoyé y resolví dudas a los estidantes acerca de el desarrollo de aplicaciones web con **Flask, Html y Sq-Lite**`,
+      },
+    ],
   }
 };
 
