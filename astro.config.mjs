b58c57ed93bee0d7e4e9f7bcf68070331d717079
LeoLizc <leolizcanop@gmail.com>
2024-02-16 00:38:48 -0500

    Add sitemap integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 import { DEFAULT_LOCALE, fallback, locales } from './src/i18n/index';
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 const SITE_URL = "https://portfolio.leolizc.tech";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: SITE_URL,
   i18n: {
     locales: locales,
